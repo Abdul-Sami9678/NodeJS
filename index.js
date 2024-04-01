2860c7ddf874fcae0546850ebde0730f596185ea
Sami Khan <samifast9678@gmail.com>
2024-04-01 04:57:44 -0700

    New Files Added

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-const http = require('http');
-http.createServer((req,resp)=>
-{
-    resp.write("<h1>Hello This Is Abdul Sami</h1>");
-    resp.end();
-}).listen(4500);
+var colors = require('colors');
+console.log("Hello".green);
