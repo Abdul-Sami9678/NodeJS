c3a5399b77ea3135bbee1bd0f22f6d2fb3de9fda
Sami Khan <samifast9678@gmail.com>
2024-04-01 04:04:22 -0700

    New Updation in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 http.createServer((req,resp)=>
 {
-    resp.write("<h1>Hello This Is Sami Khan</h1>");
+    resp.write("<h1>Hello This Is Abdul Sami</h1>");
     resp.end();
 }).listen(4500);
